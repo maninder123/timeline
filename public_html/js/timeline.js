2e9389fed85dc27341bd1333f7d44677f26b163a
Maninder Singh <manindersingh221@gmail.com>
2015-03-14 14:37:23 +0530

    top week working

--- public_html/js/timeline.js
+++ public_html/js/timeline.js
@@ -9,6 +9,55 @@
 callLogTimes = [];
 locationTimes = [];
 window.onload = function() {
+    /* This function find the current week
+     * 
+     * @version             0.0.1
+     * @since 		v0.0.1
+     * @access 		public
+     * @author 		Maninder Singh		<manindersingh221@gmail.com> 
+     */
+    var d1 = Date.parse('today');
+    var d2 = Date.parse('today');
+    if (d1.is().monday()) {
+        startDate = d1
+        endDate = d2.next().sunday();
+    }
+    else if (d1.is().sunday()) {
+        startDate = d1.last().monday();
+        endDate = d2;
+    }
+    else {
+        startDate = d1.last().monday();
+        console.log(startDate);
+        endDate = d2.next().sunday();
+    }
+    //append current date
+    //var Dates = new Date().getWeek();
+    $(".current-week-start").text(dateFormat(startDate, "dddd, mmmm dS, yyyy"));
+    $(".current-week-end").text(dateFormat(endDate, "dddd, mmmm dS, yyyy"));
+
+    $(".previousArrow").click(function() {
+        var currentWeekStart = $(".current-week-start").text();
+        var currentWeekEnd = $(".current-week-end").text();
+        var s1 = Date.parse(currentWeekStart);
+        var e1 = Date.parse(currentWeekEnd);
+        newStartDate = s1.last().monday();
+        newEndDate = e1.last().sunday();
+        $(".current-week-start").text(dateFormat(newStartDate, "dddd, mmmm dS, yyyy"));
+        $(".current-week-end").text(dateFormat(newEndDate, "dddd, mmmm dS, yyyy"));
+    });
+
+    $(".nextArrow").click(function() {
+        var currentWeekStart = $(".current-week-start").text();
+        var currentWeekEnd = $(".current-week-end").text();
+        var s1 = Date.parse(currentWeekStart);
+        var e1 = Date.parse(currentWeekEnd);
+        newStartDate = s1.next().monday();
+        newEndDate = e1.next().sunday();
+        $(".current-week-start").text(dateFormat(newStartDate, "dddd, mmmm dS, yyyy"));
+        $(".current-week-end").text(dateFormat(newEndDate, "dddd, mmmm dS, yyyy"));
+    });
+
     // reading the data from csv file
     d3.csv("data/CallLogProbe.csv", function(error1, callLogData) {
         d3.csv("data/LocationProbe.csv", function(error2, locationData) {
@@ -18,9 +67,16 @@ window.onload = function() {
                 var previousTimestamp;
                 callLogData.sort(SortByName);
                 locationData.sort(SortByName);
+                var day1 = $(".current-week-start").text();
+                var d1 = Date.parse(day1);
+                var day2 = d1.next().day();
+                var d2 = Date.parse(day2);
+                console.log(d1.getTime());
+                var startDate = d1.getTime();
+                var endDate = day2.getTime();
                 var startDate = new Date('2013-07-19 00:00:00'.split(' ').join('T')).getTime();
                 var endDate = new Date('2013-07-20 00:00:00'.split(' ').join('T')).getTime();
-
+                for(j=0; j < 7; j++){
                 $.each(callLogData, function(index, value) {
                     if (value.timestamp <= endDate / 1000 && value.timestamp >= startDate / 1000 && i > 0) {
                         var times = {
@@ -73,11 +129,11 @@ window.onload = function() {
                             })
                             .stack() // toggles graph stacking
                             .margin({left: 70, right: 30, top: 0, bottom: 0});
-                    var svg = d3.select("#timeline").append("svg").attr("width", width)
+                    var svg = d3.select("#timeline"+j).append("svg").attr("width", width)
                             .datum(labelColorTestData).call(chart);
                 }
                 timelineLabelColor();
-
+                };
             });
         });
     });
@@ -95,33 +151,4 @@ window.onload = function() {
             return 0;
         return a1 > b1 ? 1 : -1;
     }
-    /* This function find the current week
-     * 
-     * @version             0.0.1
-     * @since 		v0.0.1
-     * @access 		public
-     * @author 		Maninder Singh		<manindersingh221@gmail.com> 
-     */
-
-    Date.prototype.getWeek = function(start)
-    {
-        //Calcing the starting point
-        start = start || 0;
-        var today = new Date(this.setHours(0, 0, 0, 0));
-        var day = today.getDay() - start;
-        var date = today.getDate() - day;
-
-        // Grabbing Start/End Dates
-        var StartDate = new Date(today.setDate(date));
-        var EndDate = new Date(today.setDate(date + 6));
-        return [dateFormat(StartDate, "dddd, mmmm dS, yyyy"), dateFormat(EndDate, "dddd, mmmm dS, yyyy")];
-    }
-
-    //append current date
-    var Dates = new Date().getWeek();
-    $(".current-week").append(Dates[0] + ' - ' + Dates[1]);
-    //alert(Dates[0].toLocaleDateString() + ' - ' + Dates[1].toLocaleDateString())
-
-
 }
-

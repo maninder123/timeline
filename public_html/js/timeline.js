6950be57dc395df1da400eeb4d21487be820d989
ajeet <ajeet@pokharan.(none)>
2015-03-17 20:33:53 +0530

    read latest csv and made weekly graphical presentation

--- public_html/js/timeline.js
+++ public_html/js/timeline.js
@@ -1,119 +1,134 @@
-/**
- * This java script file reads the data from csv file and plot the timelines.
- * 
- * @version             0.0.1
- * @since 		v0.0.1
- * @access 		public
- * @author 		Maninder Singh		<manindersingh221@gmail.com> 
- */
-callLogTimes = [];
-smsTimes = [];
-window.onload = function () {
-    /* This function find the current week
-     * 
-     * @version             0.0.1
-     * @since 		v0.0.1
-     * @access 		public
-     * @author 		Maninder Singh		<manindersingh221@gmail.com> 
-     */
-    var d1 = Date.parse('today');
-    var d2 = Date.parse('today');
-    if (d1.is().monday()) {
-        startDate = d1
-        endDate = d2.next().sunday();
-    }
-    else if (d1.is().sunday()) {
-        startDate = d1.last().monday();
-        endDate = d2;
-    }
-    else {
-        startDate = d1.last().monday();
-        console.log(startDate);
-        endDate = d2.next().sunday();
-    }
-    //append current date
-    //var Dates = new Date().getWeek();
-    $(".current-week-start").text(dateFormat(startDate, "dddd, mmmm dS, yyyy"));
-    $(".current-week-end").text(dateFormat(endDate, "dddd, mmmm dS, yyyy"));
-    //---------------------------------------------------------------------------
-//appending week days
-    var day_zero = dateFormat(startDate, 'dddd, mmmm dS');
-    $('.day0').text(day_zero);
-    var i = 1;
-    for (i = 1; i < 7; i++) {
-        var week_days = startDate.next().day();
-        $('.day' + i).text(dateFormat(week_days, "dddd, mmmm dS"));
-
-    }
-//  ------------------------------------------------------------------------------
-    $(".previousArrow").click(function () {
-        var currentWeekStart = $(".current-week-start").text();
-        var currentWeekEnd = $(".current-week-end").text();
-        var s1 = Date.parse(currentWeekStart);
-        var e1 = Date.parse(currentWeekEnd);
-        newStartDate = s1.last().monday();
-        newEndDate = e1.last().sunday();
-
-        $(".current-week-start").text(dateFormat(newStartDate, "dddd, mmmm dS, yyyy"));
-
-        $(".current-week-end").text(dateFormat(newEndDate, "dddd, mmmm dS, yyyy"));
-        //appending week days
-        var day_zero_new = dateFormat(newStartDate, "dddd, mmmm dS");
-        $('.day0').text(day_zero_new);
-        var i = 1;
-        for (i = 1; i < 7; i++) {
-            var week_days_new = newStartDate.next().day();
-            $('.day' + i).text(dateFormat(week_days_new, "dddd, mmmm dS"));
-        }
-//   timelineLabelColor();
-    });
-
-    $(".nextArrow").click(function () {
-        var currentWeekStart = $(".current-week-start").text();
-        var currentWeekEnd = $(".current-week-end").text();
-        var s1 = Date.parse(currentWeekStart);
-        var e1 = Date.parse(currentWeekEnd);
-        newStartDate = s1.next().monday();
-        newEndDate = e1.next().sunday();
-        $(".current-week-start").text(dateFormat(newStartDate, "dddd, mmmm dS, yyyy"));
-        $(".current-week-end").text(dateFormat(newEndDate, "dddd, mmmm dS, yyyy"));
-        //appending week days
-        var day_zero_new1 = dateFormat(newStartDate, "dddd, mmmm dS");
-        $('.day0').text(day_zero_new1);
-        var i = 1;
-        for (i = 1; i < 7; i++) {
-            var week_days_new1 = newStartDate.next().day();
-            $('.day' + i).text(dateFormat(week_days_new1, "dddd, mmmm dS"));
-        }
-    });
-
-    // reading the data from csv file
-    d3.csv("data/CallLogProbe.csv", function (error1, callLogData) {
-        d3.csv("data/SMSProbe.csv", function (error2, smsData) {
-            d3.csv("data/ScreenProbe.csv", function (error2, screenData) {
-                // console.log(callLogData, locationData);
-                var i = 0;
-                var previousTimestamp;
-                callLogData.sort(SortByName);
-                smsData.sort(SortByName);
-                console.log(callLogData);
-                console.log(smsData);
-                var day1 = $(".current-week-start").text();
-                var d1 = Date.parse(day1);
-                var newStartDate = dateFormat(d1, "yyyy-mm-dd hh:mm:ss");
-                var day2 = d1.next().day();
-                var newEndDate = dateFormat(d2, "yyyy-mm-dd hh:mm:ss");
-//                alert(newEndDate);
-
-                var startDate = d1.getTime();
-//                console.log(startDate);
-
-                var endDate = day2.getTime();
-                var startDate = new Date('2013-07-19 00:00:00'.split(' ').join('T')).getTime();
-                var endDate = new Date('2013-07-20 00:00:00'.split(' ').join('T')).getTime();
+	/**
+	 * This java script file reads the data from csv file and plot the timelines.
+	 * 
+	 * @version             0.0.1
+	 * @since 		v0.0.1
+	 * @access 		public
+	 * @author 		Maninder Singh		<manindersingh221@gmail.com> 
+	 */
+	callLogTimes = [];
+	smsTimes = [];
+	callLogData = new Array();
+	smsData = new Array();
+	var previousTimestamp;
+	window.onload = function () {
+	    /* This function find the current week
+	     * 
+	     * @version             0.0.1
+	     * @since 		v0.0.1
+	     * @access 		public
+	     * @author 		Maninder Singh		<manindersingh221@gmail.com> 
+	     */
+	    var d1 = Date.parse('today');
+	    var d2 = Date.parse('today');
+	    if (d1.is().monday()) {
+	        startDate = d1
+	        endDate = d2.next().sunday();
+	    }
+	    else if (d1.is().sunday()) {
+	        startDate = d1.last().monday();
+	        endDate = d2;
+	    }
+	    else {
+	        startDate = d1.last().monday();
+	        //console.log(startDate);
+	        endDate = d2.next().sunday();
+	    }
+	    //append current date
+	    //var Dates = new Date().getWeek();
+	    $(".current-week-start").text(dateFormat(startDate, "dddd, mmmm dS, yyyy"));
+	    $(".current-week-end").text(dateFormat(endDate, "dddd, mmmm dS, yyyy"));
+	    //---------------------------------------------------------------------------
+	//appending week days
+	    var day_zero = dateFormat(startDate, 'dddd, mmmm dS');
+	    $('.day0').text(day_zero);
+	    var i = 1;
+	    for (i = 1; i < 7; i++) {
+	        var week_days = startDate.next().day();
+	        $('.day' + i).text(dateFormat(week_days, "dddd, mmmm dS"));
+	
+	    }
+	//  ------------------------------------------------------------------------------
+	    $(".previousArrow").click(function () {
+	    	   $.blockUI({ css: { 
+            border: 'none', 
+            padding: '15px', 
+            backgroundColor: '#000', 
+            '-webkit-border-radius': '10px', 
+            '-moz-border-radius': '10px', 
+            opacity: .5, 
+            color: '#fff' 
+        } }); 
+ 
+	        var currentWeekStart = $(".current-week-start").text();
+	        var currentWeekEnd = $(".current-week-end").text();
+	        var s1 = Date.parse(currentWeekStart);
+	        var e1 = Date.parse(currentWeekEnd);
+	        newStartDate = s1.last().monday();
+	        newEndDate = e1.last().sunday();
+	
+	        $(".current-week-start").text(dateFormat(newStartDate, "dddd, mmmm dS, yyyy"));
+	
+	        $(".current-week-end").text(dateFormat(newEndDate, "dddd, mmmm dS, yyyy"));
+	        //appending week days
+	        var day_zero_new = dateFormat(newStartDate, "dddd, mmmm dS");
+	        $('.day0').text(day_zero_new);
+	        var i = 1;
+	        for (i = 1; i < 7; i++) {
+	            var week_days_new = newStartDate.next().day();
+	            $('.day' + i).text(dateFormat(week_days_new, "dddd, mmmm dS"));
+	        }
+	          
+	        drawGraph();
+	        $.unblockUI();
+	//   timelineLabelColor();
+	    });
+	
+	    $(".nextArrow").click(function () {
+	    	
+	    	  $.blockUI();
+	        var currentWeekStart = $(".current-week-start").text();
+	        var currentWeekEnd = $(".current-week-end").text();
+	        var s1 = Date.parse(currentWeekStart);
+	        var e1 = Date.parse(currentWeekEnd);
+	        newStartDate = s1.next().monday();
+	        newEndDate = e1.next().sunday();
+	        $(".current-week-start").text(dateFormat(newStartDate, "dddd, mmmm dS, yyyy"));
+	        $(".current-week-end").text(dateFormat(newEndDate, "dddd, mmmm dS, yyyy"));
+	        //appending week days
+	        var day_zero_new1 = dateFormat(newStartDate, "dddd, mmmm dS");
+	        $('.day0').text(day_zero_new1);
+	        var i = 1;
+	        for (i = 1; i < 7; i++) {
+	            var week_days_new1 = newStartDate.next().day();
+	            $('.day' + i).text(dateFormat(week_days_new1, "dddd, mmmm dS, yyyy"));
+	        }
+	       
+	        drawGraph();
+	        $.unblockUI();
+	    });
+	    
+	    
+          var drawGraph = function (){   
+                
+                var call_log_check_count = 0;
+                var sms_check_count = 0 ;
                 for(j=0; j < 7; j++){
+                	
+                $("#timeline"+j).empty();
+                var startDateText = $(".day"+j).text();
+                var startDateObj = Date.parse(startDateText);
+                console.log(startDateText);
+                var startDate = startDateObj.getTime();
+                console.log(startDateText, startDate);
+                var endDate = startDateObj.next().day().getTime(); 
+                 
+
                 $.each(callLogData, function(index, value) {
-                    if (value.timestamp <= endDate / 1000 && value.timestamp >= startDate / 1000 && i > 0) {
+                	  //console.log( endDate,value.timestamp,startDate) ;
+                     if (value.timestamp <= endDate / 1000 && value.timestamp >= startDate / 1000 && call_log_check_count > 0) {
+                    	       //console.log( endDate) ;
+                    	        console.log( endDate,value.timestamp,startDate,previousTimestamp) ;
                             var times = {
                                 color: "green",
                                 label: "",
@@ -121,15 +136,15 @@ window.onload = function () {
                                 ending_time: value.timestamp * 1000
                             };
                             callLogTimes.push(times);
-                            console.log(times);
+                            //console.log(times);
                         }
                         previousTimestamp = value.timestamp;
-                        i++;
+                        call_log_check_count++;
                     });
 
                     $.each(smsData, function (index, value) {
                         //console.log(value.timestamp);
-                        if (value.timestamp <= endDate / 1000 && value.timestamp >= startDate / 1000 && i > 0) {
+                        if (value.timestamp <= endDate / 1000 && value.timestamp >= startDate / 1000 && sms_check_count > 0) {
                             var times = {
                                 color: "pink",
                                 label: "",
@@ -139,7 +154,7 @@ window.onload = function () {
                             smsTimes.push(times);
                         }
                         previousTimestamp = value.timestamp;
-                        i++;
+                        sms_check_count++;
                     });
 
 
@@ -147,7 +162,7 @@ window.onload = function () {
                         {id: "callLog", label: "Call Log", times: callLogTimes},
                         {id: "sms", label: "SMS", times: smsTimes},
                     ];
-                    console.log(labelColorTestData);
+                   // console.log(labelColorTestData);
 
                     var width = 700;
 
@@ -170,22 +185,41 @@ window.onload = function () {
                     }
                     timelineLabelColor();
                 }
-                ;
-            });
-        });
-    });
-
-    /* This function will sort the data on timestamp
-     * 
-     * @version             0.0.1
-     * @since 		v0.0.1
-     * @access 		public
-     * @author 		Maninder Singh		<manindersingh221@gmail.com> 
-     */
-    function SortByName(a, b) {
-        var a1 = a.timestamp, b1 = b.timestamp;
-        if (a1 == b1)
-            return 0;
-        return a1 > b1 ? 1 : -1;
-    }
-}
+                }
+			
+			    // reading the data from csv file
+			    d3.csv("data/CallLogProbe.csv", function (error1, callLog) {
+			        d3.csv("data/SMSProbe.csv", function (error2, smsLog) {
+			            d3.csv("data/ScreenProbe.csv", function (error2, screenData) {
+			                // console.log(callLogData, locationData);
+			                //var i = 0;
+			                // var previousTimestamp;
+			                callLogData = callLog.sort(SortByName);
+			                smsData = smsLog.sort(SortByName);
+			                //onsole.log(callLogData);
+			                //console.log(smsData);
+			                var day1 = $(".current-week-start").text();
+			                var d1 = Date.parse(day1);
+			                var newStartDate = dateFormat(d1, "yyyy-mm-dd hh:mm:ss");
+			                var day2 = d1.next().day();
+			                var newEndDate = dateFormat(d2, "yyyy-mm-dd hh:mm:ss");
+			                drawGraph();
+			  
+			            });
+			        });
+			    });
+	
+	    /* This function will sort the data on timestamp
+	     * 
+	     * @version             0.0.1
+	     * @since 		v0.0.1
+	     * @access 		public
+	     * @author 		Maninder Singh		<manindersingh221@gmail.com> 
+	     */
+	    function SortByName(a, b) {
+	        var a1 = a.timestamp, b1 = b.timestamp;
+	        if (a1 == b1)
+	            return 0;
+	        return a1 > b1 ? 1 : -1;
+	    }
+	}
